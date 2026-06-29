baa7bae63ea74cc9ad0c225201f2e67421da79c4
Diego-Manriquez-DUOC <dieg.manriquez@duocuc.cl>
2026-06-28 22:27:39 -0400

    test sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,10 +106,10 @@ resource "aws_security_group" "rds_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_cluster_sg.id]
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
